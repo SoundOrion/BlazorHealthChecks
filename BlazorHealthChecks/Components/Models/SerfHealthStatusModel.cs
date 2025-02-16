d95e1a9236883e018f47b4bcd37a5d17f00ae210
hatsuyama <hatsuyama>
2025-02-16 09:40:46 +0900

    add

---
+++ BlazorHealthChecks/Components/Models/SerfHealthStatusModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorHealthChecks.Models;
+
+public class HealthCheckModel
+{
+    public string Name { get; set; }
+    public bool IsHealthy { get; set; }
+    public DateTime OnStateFrom { get; set; }
+    public DateTime LastExecution { get; set; }
+}
